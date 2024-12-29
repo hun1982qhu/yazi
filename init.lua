36530c104ee1ba9c35a82f5d762a91714df2b55a
H.N <hun1982@qq.com>
2024-12-29 08:02:47 +0100

    202412290802

--- init.lua
+++ init.lua
@@ -39,3 +39,45 @@ require("bunny"):setup({
 	notify = true, -- notify after hopping, default is false
 	fuzzy_cmd = "sk", -- fuzzy searching command, default is fzf
 })
+-- ~/.config/yazi/init.lua
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%b %d %H:%M", time)
+	else
+		time = os.date("%b %d  %Y", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end
+require("copy-file-contents"):setup({
+	append_char = "\n",
+	notification = true,
+})
+require("git"):setup()
+if os.getenv("NVIM") then
+	require("hide-preview"):entry()
+end
+require("restore"):setup({
+	-- Set the position for confirm and overwrite dialogs.
+	-- don't forget to set height: `h = xx`
+	-- https://yazi-rs.github.io/docs/plugins/utils/#ya.input
+	position = { "center", w = 70, h = 40 },
+
+	-- Show confirm dialog before restore.
+	-- NOTE: even if set this to false, overwrite dialog still pop up
+	show_confirm = true,
+
+	-- colors for confirm and overwrite dialogs
+	theme = {
+		title = "blue",
+		header = "green",
+		-- header color for overwrite dialog
+		header_warning = "yellow",
+		list_item = { odd = "blue", even = "blue" },
+	},
+})
+require("dual-pane"):setup()
