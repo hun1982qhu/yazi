3f220f2927ed0450cc013c565fdd1664afbb1249
H.N. <hun1982@qq.com>
2025-02-25 00:30:21 +0100

    202502250029

--- init.lua
+++ init.lua
@@ -1,14 +1,12 @@
 -- ~/.config/yazi/init.lua
-
 require("bookmarks"):setup({
-	save_last_directory = true, -- DEPRECATED - will be removed in the future. Use `last_directory`
-	last_directory = { enable = true, persist = true },
-	persist = "all",
+	last_directory = { enable = false, persist = false },
+	persist = "none",
 	desc_format = "full",
 	file_pick_mode = "hover",
 	notify = {
-		enable = true,
-		timeout = 3,
+		enable = false,
+		timeout = 1,
 		message = {
 			new = "New bookmark '<key>' -> '<folder>'",
 			delete = "Deleted bookmark in '<key>'",
@@ -81,3 +79,40 @@ require("restore"):setup({
 	},
 })
 require("dual-pane"):setup()
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+if ya.target_family() == "windows" then
+	table.insert(bookmarks, {
+		tag = "Scoop Local",
+
+		path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+		key = "p",
+	})
+	table.insert(bookmarks, {
+		tag = "Scoop Global",
+		path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+		key = "P",
+	})
+end
+table.insert(bookmarks, {
+	tag = "Desktop",
+	path = home_path .. path_sep .. "Desktop" .. path_sep,
+	key = "d",
+})
+
+require("yamb"):setup({
+	-- Optional, the path ending with path seperator represents folder.
+	bookmarks = bookmarks,
+	-- Optional, recieve notification everytime you jump.
+	jump_notify = true,
+	-- Optional, the cli of fzf.
+	cli = "fzf",
+	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	-- Optional, the path of bookmarks
+	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
+		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+})
